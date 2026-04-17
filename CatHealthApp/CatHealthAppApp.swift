7df0e55765047048cba4158652cd8aba68cbe57d
Jingyan <ljingyan556@gmail.com>
2026-04-16 19:10:31 -0700

    Initial Commit

---
+++ CatHealthApp/CatHealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatHealthAppApp.swift
+//  CatHealthApp
+//
+//  Created by carl li on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct CatHealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
